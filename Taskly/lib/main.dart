1b5ef9b97ff7098d6a61cb5b687fca3daf1975c1
Sailesh aryal <sailesharyal500@gmail.com>
2024-07-10 12:26:12 +0545

    started a new projects

---
+++ Taskly/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:taskly/Pages/home_pages.dart';
+
+void main() {
+  runApp(const Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  const Myapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Taskly',
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: HomePages(),
+    );
+  }
+}
