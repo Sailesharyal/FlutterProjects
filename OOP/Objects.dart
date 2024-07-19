512b699c7d3bb36d44585fd558a8bb15313d45fe
Sailesh aryal <sailesharyal500@gmail.com>
2024-07-19 12:30:38 +0545

    Creating a objects from the previous class

---
+++ OOP/Objects.dart
@@ -0,0 +1,13 @@
+import 'ClassAndObject.dart';
+
+void main() {
+  Student student = new Student();
+  student.name = "sailesh";
+  student.age = 19;
+  student.display();
+
+  Bike bike = new Bike();
+  bike.name = "Royal Enfield";
+  bike.NumOfTyre = 2;
+  bike.OnStart();
+}
