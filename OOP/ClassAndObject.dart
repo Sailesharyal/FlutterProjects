2a3964f9608df14900a4d7be0edd3defb62b11b8
Sailesh aryal <sailesharyal500@gmail.com>
2024-07-19 12:16:31 +0545

    learning about OOP in Flutter

---
+++ OOP/ClassAndObject.dart
@@ -0,0 +1,20 @@
+class Student {
+  //Making  a class
+  String? name;
+  int? age; //Creating a class    #Declaring a properties  of a class
+
+  void display() {
+    print(
+        "My name is $name and  my age is $age"); // Declaring a methods of a class
+  }
+}
+
+class Car {
+  String? name;
+  String? VehicleType;
+  int? NumOfTyre;
+
+  void OnStart() {
+    print("This is a $name car and this car have $NumOfTyre");
+  }
+}
